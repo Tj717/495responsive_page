5a2ca754764608392b9b7668d9d2d3ac423c89c1
Tai Jing <jingtai@uw.edu>
2022-10-30 17:09:32 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-let icon = document.getElementsByClassName("icon")[0];
 
+// document.getElementById('myTopnav').innerHTML = '<a href="index.html">Abstract</a><a href="Idea1.html">Idea 1</a><a class="active" href="Idea2.tml">Idea 2</a><a href="Idea3.html">Idea 3</a><a href="see_also.html">See Also</a><a href="references.html">References</a><a href="javascript:void(0);" class="icon"><i class="fa fa-bars"></i></a>';
+
+let icon = document.getElementsByClassName("icon")[0];
 icon.addEventListener('click', responsive_control);
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
@@ -11,3 +13,4 @@ function responsive_control() {
     x.className = "topnav";
   }
 }
+
